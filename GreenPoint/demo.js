d66fbb34838cc6d4153e7adb13da99ea0a70c060
Legendary_Folah <Folafad@gmail.com>
2021-09-20 17:44:47 +0100

    removed _ and space

--- GreenPoint/demo.js
+++ GreenPoint/demo.js
@@ -1,4 +1,4 @@
-const sign_in_btn = document.querySelector('#sign-in-btn');
+const signin_btn = document.querySelector('#sign-in-btn');
 const sign_up_btn = document.querySelector('#sign-up-btn');
 const container = document.querySelector('.container');
 
